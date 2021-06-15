6fa1392aa37c1426e82f919d78f8b82892fe37a9
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-06-15 19:05:01 -0400

    fix deepmodeling/deepmd-kit#730

---
+++ run.sh
@@ -0,0 +1,7 @@
+set -e
+./build-locally.py linux_64_cuda_compiler_version11.1float_prechighpython3.9._
+./build-locally.py linux_64_cuda_compiler_version11.1float_prechighpython3.8._
+./build-locally.py linux_64_cuda_compiler_version11.1float_prechighpython3.7._
+./build-locally.py linux_64_cuda_compiler_version11.1float_preclowpython3.9._
+./build-locally.py linux_64_cuda_compiler_version11.1float_preclowpython3.8._
+./build-locally.py linux_64_cuda_compiler_version11.1float_preclowpython3.7._
